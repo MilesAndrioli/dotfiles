be1ac0d0d1d701928adc1ecbb2445a7091adfc62
Miles Andrioli <98649838+MilesAndrioli@users.noreply.github.com>
2026-03-21 16:17:35 +0100

    replaced alacritty with wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,31 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.default_prog = { 'wsl.exe', '-d', 'Ubuntu', '--cd', '~' }
+
+config.win32_system_backdrop = 'Acrylic'
+config.window_background_opacity = 0.6
+config.color_scheme = 'Catppuccin Mocha'
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 10
+config.line_height = 1.2
+
+config.window_padding = {
+    left = '1cell',
+    right = '1cell',
+    top = '0.5cell',
+    bottom = '0.5cell',
+}
+
+config.hide_tab_bar_if_only_one_tab = true
+
+config.keys = {
+    { key = 't', mods = 'CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+    { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentTab { confirm = false } },
+    { key = 'e', mods = 'CTRL', action = wezterm.action.ActivateTabRelative(1) },
+    { key = 'q', mods = 'CTRL', action = wezterm.action.ActivateTabRelative(-1) },
+}
+
+return config
