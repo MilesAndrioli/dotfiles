8e729bea92d04c3f0d4cec94ae7cb7ca8c19ff27
Miles <98649838+MilesAndrioli@users.noreply.github.com>
2026-04-15 17:41:15 +0200

    Update .wezterm.lua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,14 +4,14 @@ local config = wezterm.config_builder()
 config.default_domain = 'WSL:Ubuntu'
 
 config.win32_system_backdrop = 'Acrylic'
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.7
 config.color_scheme = 'Catppuccin Mocha'
 -- config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font 'MonoLisa'
 config.font_size = 10
-config.line_height = 1.2
+config.line_height = 1.1
 
 config.window_padding = {
     left = '1cell',
@@ -19,7 +19,6 @@ config.window_padding = {
     top = '0.5cell',
     bottom = '0.5cell',
 }
-
 
 config.keys = {
     { key = 't', mods = 'CTRL', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
